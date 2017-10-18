742a8887683d75382a9d20b4ba48343a3a066f48
seeksoul <kamil.fijalski@outlook.com>
2017-10-18 23:42:40 +0200

    Dict methods

--- 10 - Słowniki.py
+++ 10 - Słowniki.py
@@ -1,6 +1,6 @@
 birthdays = {'Kamil': '14 Kwi', 'Pati': '24 Lip'}
 
-while name != '':
+while True:
     print('Enter a name: ')
     name = input()
     if name == '':
@@ -8,9 +8,21 @@ while name != '':
 
     if name in birthdays:
         print(birthdays[name] + ' is the birthday of ' + name)
+        break
     else:
         print('I do not have information about ' + name)
         print('Could you tell me when is your birthday? ')
         bday = input()
         birthdays[name] = bday
         print('Birthday database was updated')
+        break
+
+print('.')
+for i in birthdays.values():
+    print(i)
+print('.')
+for i in birthdays.keys():
+    print(i)
+print('.')
+for i in birthdays.items():
+    print(i)
