bd79ac7da4e4f1c7661db3dfadf3c4b0bbad27e7
kamil-fijalski <kamil.fijalski@outlook.com>
2017-10-19 21:48:57 +0200

    First Commit

---
+++ 11 - DecToBinary.py
@@ -0,0 +1,17 @@
+print('Type number in decimal system: ')
+num = int(input())
+
+num_temp = num
+end_string = ''
+
+while num_temp > 1:
+    next_num = num_temp%2
+    end_string = end_string + str(next_num)
+    num_temp = int((num_temp-next_num)/2)
+
+if num_temp == 1:
+    end_string = end_string + '1'
+
+print('Your number is: ' + str(num))
+print('In binary system it is: ' + end_string[::-1])
+
